3884632c381021ba3a34db433779fbd04efbcbeb
hheyyeon21 <mhygood06@gmail.com>
2025-04-10 11:12:43 +0900

    NPC punch script

---
+++ Assets/Scenes/heeyeon/Script/NPCpunch.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class NPCpunch : MonoBehaviour
+{
+    public float launchForceUp = 2000f;
+    public float launchForceForward = 500f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+
+                if (rb != null) //
+                {
+                    Vector3 launchDirection = Camera.main.transform.forward * launchForceForward + Vector3.up * launchForceUp;
+                    rb.AddForce(launchDirection);
+                }
+            }
+        }
+    }
+}
