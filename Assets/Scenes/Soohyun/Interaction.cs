9085da96b8cd402fb8768bd18ae0d6f721b36dd6
tnqkr3658 <tnqkr3658@gmail.com>
2025-04-17 13:08:35 +0900

    Button

--- Assets/Scenes/Soohyun/Interaction.cs
+++ Assets/Scenes/Soohyun/Interaction.cs
@@ -5,20 +5,44 @@ public class Interaction : MonoBehaviour
     public static bool gameStart = false;
     public GameObject Player;
     public GameObject Table;
+    public GameObject Button;
+    public GameObject Slider1;
+    public GameObject Slider2;
     float Dist;
+    bool On = false;
     // Update is called once per frame
+    void Start()
+    {
+        Slider1.SetActive(false);
+        Slider2.SetActive(false);
+    }
     void Update()
     {
         if (!gameStart)
         {
+            Button_on();
             Dist = Vector3.Distance(Player.transform.position, Table.transform.position);
-            if (Input.GetKey("f") && Dist < 3)
+            if (Dist < 3)
             {
-                Debug.Log("클릭! 거리:" + Dist);
-                gameStart = true;
-                Cursor.visible = true;
+                On = true;
+                if (Input.GetKey("f"))
+                {
+                    gameStart = true;
+                    Cursor.visible = true;
+                    On = false;
+                    Slider1.SetActive(!On);
+                    Slider2.SetActive(!On);
+                }
+            }
+            else
+            {
+                On = false;
             }
         }
+    }
+    void Button_on()
+    {
+        Button.SetActive(On);
     }
 
 }
