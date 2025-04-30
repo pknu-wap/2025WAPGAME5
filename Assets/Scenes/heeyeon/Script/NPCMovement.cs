108bcd854e80f3ee76565bb3195d2a3493635f8b
hheyyeon21 <mhygood06@gmail.com>
2025-04-30 16:55:16 +0900

    stuck in the ground

--- Assets/Scenes/heeyeon/Script/NPCMovement.cs
+++ Assets/Scenes/heeyeon/Script/NPCMovement.cs
@@ -4,13 +4,13 @@ public class NPCMovement : MonoBehaviour
 {
     public float moveSpeed = 2f;
     public bool isnotStudent = true;
-
     private Vector3 moveDirection;
 
     void Start()
     {
         moveDirection = isnotStudent ? transform.forward : -transform.forward;
     }
+
 
     void Update()
     {
