b625e85b1dd018cd8ef80c7e21b4949d91f9d30e
hheyyeon21 <mhygood06@gmail.com>
2025-05-10 00:05:59 +0900

    [수학] 게임 테스트

--- Assets/Scenes/heeyeon/1_NPCpunching/MathTrigger.cs
+++ Assets/Scenes/heeyeon/1_NPCpunching/MathTrigger.cs
@@ -11,13 +11,4 @@ public class MathTrigger : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        if (PlayerPrefs.HasKey("MathTime"))
-        {
-            float mathTime = PlayerPrefs.GetFloat("MathTime");
-            Debug.Log("수학 게임 총 플레이 시간: " + mathTime.ToString("F2") + "초");
-            PlayerPrefs.DeleteKey("MathTime");
-        }
-    }
 }
