51be06d4e90a959bf07f4966d2f11a2c961e6a53
tnqkr3658 <tnqkr3658@gmail.com>
2025-05-11 16:07:44 +0900

    드래그 앤 드롭

---
+++ Assets/Scenes/Soohyun/drag.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector3 defaultPos;
+    public Canvas parentCanvas;
+    //클론
+    private GameObject cloneObject;
+    private RectTransform cloneRect;
+    private CanvasGroup cloneCanvasGroup;
+    int a=1;
+    void Awake()
+    {
+        if (parentCanvas == null)
+        {
+            parentCanvas = GetComponentInParent<Canvas>();
+        }
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+        defaultPos = rectTransform.anchoredPosition;
+        // 복제본 생성
+        cloneObject = Instantiate(this.gameObject, parentCanvas.transform);
+        cloneObject.name = this.name;
+        cloneRect = cloneObject.GetComponent<RectTransform>();
+        cloneCanvasGroup = cloneObject.GetComponent<CanvasGroup>();
+
+        // 복제본만 드래그 가능하게 설정
+        cloneCanvasGroup.blocksRaycasts = true;
+
+        // 원본은 그대로, clone만 드래그
+        cloneRect.position = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / parentCanvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        cloneCanvasGroup.blocksRaycasts = true;
+        //rectTransform.anchoredPosition = defaultPos;
+    }
+}
