aa57c5fedeecfda51b9e66e95ad416ab0f4106dc
hheyyeon21 <mhygood06@gmail.com>
2025-05-19 23:16:21 +0900

    [과학] 달걀 이미지 넣

--- Assets/Scenes/heeyeon/3_Science/DistanceMeter.cs
+++ Assets/Scenes/heeyeon/3_Science/DistanceMeter.cs
@@ -4,11 +4,16 @@ using TMPro;
 public class DistanceMeter : MonoBehaviour
 {
     public Transform targetLine;  
-    public TextMeshProUGUI scoreText; 
+    public TextMeshProUGUI scoreText;
+    public Transform egg; 
 
     public void CalculateScore(Vector3 eggPosition)
     {
-        float rawDistance = Mathf.Abs(eggPosition.y - targetLine.position.y);
+        float eggBottomY = eggPosition.y - (egg.GetComponent<SpriteRenderer>().bounds.size.y / 2f); // 달걀은 아래 기준
+        float topOfLineY = targetLine.position.y + (targetLine.GetComponent<SpriteRenderer>().bounds.size.y / 2f); // 라인은 윗 선
+
+        float rawDistance = Mathf.Abs(eggBottomY - topOfLineY);
+
         float distanceInCm = rawDistance * 0.1f * 0.5f;
         float maxDistance = 0.5f;
         float score = Mathf.Max(0f, (1f - rawDistance / maxDistance) * 100f);
