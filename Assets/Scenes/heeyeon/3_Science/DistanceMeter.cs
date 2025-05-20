09c565fb053eaf0ef468f2f14003c9c2db6c4064
hheyyeon21 <mhygood06@gmail.com>
2025-05-20 23:31:53 +0900

    [과학] 스테이지 1

--- Assets/Scenes/heeyeon/3_Science/DistanceMeter.cs
+++ Assets/Scenes/heeyeon/3_Science/DistanceMeter.cs
@@ -3,21 +3,57 @@ using TMPro;
 
 public class DistanceMeter : MonoBehaviour
 {
-    public Transform targetLine;  
+    public Transform targetLine;
     public TextMeshProUGUI scoreText;
-    public Transform egg; 
+    public Transform egg;
+
+    private SpriteRenderer eggRenderer;
+    private SpriteRenderer lineRenderer;
+
+    private bool hasFinished = false;
+
+    void Start()
+    {
+        eggRenderer = egg.GetComponent<SpriteRenderer>();
+        lineRenderer = targetLine.GetComponent<SpriteRenderer>();
+    }
 
     public void CalculateScore(Vector3 eggPosition)
     {
-        float eggBottomY = eggPosition.y - (egg.GetComponent<SpriteRenderer>().bounds.size.y / 2f); // 달걀은 아래 기준
-        float topOfLineY = targetLine.position.y + (targetLine.GetComponent<SpriteRenderer>().bounds.size.y / 2f); // 라인은 윗 선
+        float eggTopY = eggPosition.y + (eggRenderer.bounds.size.y / 2f);
+        float eggBottomY = eggPosition.y - (eggRenderer.bounds.size.y / 2f);
 
-        float rawDistance = Mathf.Abs(eggBottomY - topOfLineY);
+        float lineTopY = targetLine.position.y + (lineRenderer.bounds.size.y / 2f);
+        float lineBottomY = targetLine.position.y - (lineRenderer.bounds.size.y / 2f);
 
-        float distanceInCm = rawDistance * 0.1f * 0.5f;
+        float minY = lineBottomY + (eggRenderer.bounds.size.y / 2f);
+        if (eggPosition.y < minY)
+        {
+            egg.position = new Vector3(eggPosition.x, minY, eggPosition.z);
+            eggBottomY = minY - (eggRenderer.bounds.size.y / 2f);
+        }
+
+        if (eggTopY > lineTopY)
+        {
+            scoreText.text = "실패! 선을 넘었습니다.";
+            hasFinished = true;
+            return;
+        }
+
+        float rawDistance = lineTopY - eggBottomY;
         float maxDistance = 0.5f;
-        float score = Mathf.Max(0f, (1f - rawDistance / maxDistance) * 100f);
+        float distanceInCm = rawDistance * 100f;
+        float score = Mathf.Clamp01(1f - rawDistance / maxDistance) * 100f;
 
-        scoreText.text = $"{distanceInCm:F1}cm 남기고 멈췄습니다!\n점수: {score:F1}";
+        if (Mathf.Approximately(rawDistance, 0f))
+        {
+            scoreText.text = $"딱 맞췄습니다!\n점수: 100.0";
+        }
+        else
+        {
+            scoreText.text = $"{distanceInCm:F1}cm 남기고 멈췄습니다!\n점수: {score:F1}";
+        }
+
+        hasFinished = true;
     }
 }
