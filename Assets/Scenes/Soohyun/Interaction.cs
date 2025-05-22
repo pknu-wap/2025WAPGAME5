e7bdec15b57e092d8967ac382c854f21bf791db4
tnqkr3658 <tnqkr3658@gmail.com>
2025-05-22 14:34:47 +0900

    게임 시작할 때 플레이어 고정

--- Assets/Scenes/Soohyun/Interaction.cs
+++ Assets/Scenes/Soohyun/Interaction.cs
@@ -31,7 +31,10 @@ public class Interaction : MonoBehaviour
                 if (Input.GetKey("f"))
                 {
                     gameStart = true;
-                    Player.transform.position = new Vector3(-1, 6, -48);
+                    Player.transform.position = new Vector3(-1, 6, -48); 
+                    Rigidbody rb = GetComponent<Rigidbody>();
+                    rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+                    rb.constraints = RigidbodyConstraints.None;
                     repeat = false;
                     //Cursor.visible = true;
                     Slider1.SetActive(On);
