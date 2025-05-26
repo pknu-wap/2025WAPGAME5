93cc2a95dad268fac21754b98f62b6e73140dc42
tnqkr3658 <tnqkr3658@gmail.com>
2025-05-27 01:17:17 +0900

    블럭 코딩 후 움직임 구현

--- Assets/Scenes/Soohyun/computer/connect.cs
+++ Assets/Scenes/Soohyun/computer/connect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.Rendering.DebugUI;
 
 public class connect : MonoBehaviour
 {
@@ -11,50 +12,115 @@ public class connect : MonoBehaviour
     public float moveSpeed = 10f;
     public RectTransform start;
     public static Vector2 startpos;
-    public bool gameStart   =false ;
+    public bool canStart =true ;
+    public bool running = true;
+    public List<Transform> children = new List<Transform>();
 
     //bool isMoving = false;
     //bool isRotating = false;
     //List<GameObject> children =new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || gameStart)
+        if (Input.GetKeyDown(KeyCode.Space) && canStart)
         {
-            //if (!isMoving && !isRotating) {
+            
+            canStart = false;
             foreach (Transform child in start.transform.GetComponentsInChildren<Transform>())
             {
-                Debug.Log(child.name);
-                Vector3 childPos = child.transform.position;
-                canvas.SetActive(false);
-                StartCoroutine(ActionCoroutine(child));
-                gameStart = false;
+                if (child != transform)  // 자기 자신은 제외
+                {
+                    children.Add(child);
+                }
             }
-            canvas.SetActive(true);
+            StartCoroutine(ActionCoroutine());
+            Debug.Log("끝");
         }
     }
-    IEnumerator ActionCoroutine(Transform child)
+    IEnumerator ActionCoroutine()
     {
-        yield return new WaitForSeconds(1f);
+        Debug.Log("시작");
 
-        if (child.tag == "forward")
+        
+        Debug.Log(children);
+        foreach (Transform child in children)
         {
-            transform.position += transform.forward;
-            Debug.Log("앞으로");
-        }
-        else if (child.tag == "left")
-        {
-            Debug.Log("왼쪽으로");
-            transform.eulerAngles += new Vector3(0f, -90f, 0f);
+            Debug.Log(start.transform.childCount);
+            if (child == start.transform)
+            {
+                continue;
+            }
+            canvas.SetActive(false);
+            Debug.Log(child.name);
+            Vector3 startpos = robot.transform.position;
+            float movedistance = 0f;
+            float totalangle = 0f;
+            float moveangle = 0f;
+            while (running)
+            {
 
-        }
-        else if (child.tag == "right")
-        {
-            Debug.Log("오른쪽으로");
-            transform.eulerAngles += new Vector3(0f, 90f, 0f);
+                if (child.tag == "forward")
+                {
+                    robot.transform.position += robot.transform.forward * moveSpeed * Time.deltaTime;
+                    //10만큼 이동하면 중지
+                    movedistance = Vector3.Distance(robot.transform.position, startpos);
+                    if (movedistance > 10)
+                    {
+                        running = false;
+                        Vector3 direction = (robot.transform.position - startpos).normalized;
+                        Debug.Log(direction);
+                        robot.transform.position = startpos + direction * 10;
+                    }
+                    yield return null;
 
+                }
+                else if (child.tag == "left")
+                {
+                    moveangle = 90 * Time.deltaTime;
+                    transform.Rotate(0, -moveangle, 0);
+                    totalangle -= moveangle;
+                    if (totalangle < -90)
+                    {
+                        running = false;
+                        float yangle = Mathf.FloorToInt(Mathf.Abs(robot.transform.eulerAngles.y));
+                        if ((yangle%90)!=0)
+                            {
+                            yangle = Mathf.FloorToInt(yangle / 90) * 90 + 90;
+                            }
+
+                        robot.transform.eulerAngles = new Vector3(0, yangle, 0);
+                    }
+                    yield return null;
+
+                }
+                else if (child.tag == "right")
+                {
+                    moveangle = 90 * Time.deltaTime;
+                    transform.Rotate(0, moveangle, 0);
+                    totalangle += moveangle;
+                    if (totalangle > 90)
+                    {
+                        running = false;
+                        robot.transform.eulerAngles = new Vector3(0, Mathf.FloorToInt(robot.transform.eulerAngles.y), 0);
+                    }
+                    yield return null;
+
+                }
+                else
+                {
+                    // 예외 처리 또는 대기
+                    Debug.LogWarning($"알 수 없는 태그: {child.tag}");
+                    running = false;  // 잘못된 태그일 경우 루프 종료 (또는 yield return null;)
+                    yield return null;
+                }
+            }
+            Debug.Log(child+"끝");
+            yield return new WaitForSeconds(0.5f);
+            running = true ;
         }
+        canStart = true;
+        canvas.SetActive(true);
+        children.Clear();
     }
 }
