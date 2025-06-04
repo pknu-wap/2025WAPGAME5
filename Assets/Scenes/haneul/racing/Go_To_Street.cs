b26d189817f2ef2ab891b4ec9bf48a81fb8d1da3
skypukyong <didani1234@naver.com>
2025-06-04 23:08:10 +0900

    브금변경,버튼소리추가,터널추가 및 씬 전환 제작완료

--- Assets/Scenes/haneul/racing/Go_To_Street.cs
+++ Assets/Scenes/haneul/racing/Go_To_Street.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Go_To_Street : MonoBehaviour
 {
-    private float startTime;      
-    private bool timerStarted = false; 
+    private float startTime;
+    private bool timerStarted = false;
+    public Image fadeImage;
+    public float fadeDuration = 1f;
 
     void Start()
     {
@@ -15,20 +18,36 @@ public class Go_To_Street : MonoBehaviour
         Debug.Log("타이머 시작");
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Car") && timerStarted)
+        if (other.gameObject.layer == LayerMask.NameToLayer("Car") && timerStarted)
         {
             float elapsedTime = Time.time - startTime;
 
-            Debug.Log($"2단계 자동차:{elapsedTime:F2}초");
+            Debug.Log($"2단계 자동차 트리거:{elapsedTime:F2}초");
 
             GameManager.currentMission += 1;
-
             timerStarted = false;
-
             GameManager.Instance.racingTime = elapsedTime;
-            SceneManager.LoadScene("heeyeon");
+
+            StartCoroutine(FadeOut("heeyeon"));
         }
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        fadeImage.gameObject.SetActive(true);
+        float t = 0f;
+        Color color = fadeImage.color;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            color.a = t / fadeDuration;
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 }
